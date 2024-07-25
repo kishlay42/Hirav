06186c72183b799f9d5fe6719e782b68eb5506ff
kishlay42 <kishlayk7357@gmail.com>
2024-07-25 11:31:21 +0530

    refactor: Update static file serving path to use "/client/dist"

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.listen(3000, () => {
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'client','dist','index.html'));
